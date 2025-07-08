6ae5d0fd7484807f8a6217cadbb41af4d534237f
Ali-Minejem <minejem.ali@gmail.com>
2025-07-09 00:39:26 +0100

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
-
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  site: "https://www.riadmaisondutresor.com",
-  integrations: [tailwind(), sitemap(), react()],
+  site: "https://www.maisondutresormarrakech.com",
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
+  integrations: [tailwind(), sitemap(), react()],
 });
