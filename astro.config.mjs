df3ee0e19b2447cdd3b0cf7ba7556a134266bd75
Ali-Minejem <minejem.ali@gmail.com>
2025-07-09 19:46:36 +0100

    add wrangler conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
-import vercel from "@astrojs/vercel/serverless";
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: "https://www.maisondutresormarrakech.com",
   output: "server",
-  adapter: vercel(),
-  integrations: [tailwind(), react()],
+
+  integrations: [tailwind(), react(), sitemap()],
+
+  adapter: cloudflare(),
 });
