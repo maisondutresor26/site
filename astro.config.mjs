01573a76e7707a36f1be9b458bc4245cd90f6ca4
Ali-Minejem <minejem.ali@gmail.com>
2025-07-14 23:26:51 +0100

    last modif

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://www.maisondutresormarrakech.com",
-  output: "server",
-
-  integrations: [tailwind(), react(), sitemap()],
-
+  output: "static",
   adapter: cloudflare(),
+  integrations: [tailwind(), react(), sitemap()],
 });
